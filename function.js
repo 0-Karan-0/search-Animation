f49eeb40d42cac44a831a3ed8d1199acd681545e
0-Karan-0 <94389978+0-Karan-0@users.noreply.github.com>
2021-12-26 14:06:38 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,5 @@
+var searchBox = document.getElementById("searchBox");
+var instaIcon = document.getElementById("instaIcon");
+instaIcon.onclick = function(){
+    searchBox.classList.toggle("active");
+}
